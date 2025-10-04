d3132df024a709a2ba1a7bf532997957f0db80ff
Georgi Komov <69540742+gkomov123@users.noreply.github.com>
2025-10-04 06:00:54 +0300

    fixed a small bug with the printing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import random
 
-MAX_LINES = 3 # Max amount of lines
+MAX_LINES = 3                                           # Max amount of lines
 MAX_BET = 100
 MIN_BET = 1
 
@@ -17,15 +17,15 @@ symbol_count = {
 
 def get_slot_machine_spin(rows, cols, symbols):
     all_symbols = []
-    for symbol, symbol_count in symbols.items(): # Loop thru the dictionaries key and values
-        for _ in range(symbol_count): # Iterate n times for the symbol count/ A->2->iterate 2 times
-            all_symbols.append(symbol) # Append every itteration to all_symbols
+    for symbol, symbol_count in symbols.items():        # Loop thru the dictionaries key and values
+        for _ in range(symbol_count):                   # Iterate n times for the symbol count/ A->2->iterate 2 times
+            all_symbols.append(symbol)                  # Append every itteration to all_symbols
 
     columns = []
-    for _ in range(cols): #Generate a column for every single column we have
+    for _ in range(cols):                               #Generate a column for every single column we have
         column = []
-        current_symbols = all_symbols[:] # Make a copy of the symbols list 
-        for _ in range(rows): # Loop thru the number of values we need to generate which is the number of rows
+        current_symbols = all_symbols[:]                # Make a copy of the symbols list 
+        for _ in range(rows):                           # Loop thru the number of values we need to generate which is the number of rows
             value = random.choice(current_symbols)
             current_symbols.remove(value)
             column.append(value)
@@ -35,13 +35,13 @@ def get_slot_machine_spin(rows, cols, symbols):
     return columns
 
 
-def print_slot_machine(columns):
-    for row in range(len(columns[0])): #Loop thru the lenght of the columns thus making rows
-        for i, column in enumerate(columns): # loop and enumerate thru every column
-            if i != len(columns - 1): # Cheks if its the last item
-                print(column[row], "|") # Prints every first item on the column thus making a row
+def print_slot_machine(columns):                        # Transpose the columns
+    for row in range(len(columns[0])):                  #Loop thru the lenght of the columns thus making rows
+        for i, column in enumerate(columns):            # loop and enumerate thru every column
+            if i != len(columns) - 1:                   # Cheks if its the last item
+                print(column[row], end=" | ")                 # Prints every first item on the column thus making a row
             else:
-                print(column[row]) # Prints without "|" if its the last item
+                print(column[row], end="")                      # Prints without "|" if its the last item
 
 
 
@@ -96,12 +96,15 @@ def main():
     while True:
         bet = get_bet_amount()
         total_bet = bet * lines
-        if total_bet <= balance: # Checks if the total bet is greater than the deposit
+        if total_bet <= balance:                        # Checks if the total bet is greater than the deposit
             break
         else:
             print(f"You do not have enough to bet that amount, balance: ${balance}")
 
  
     print(f"You are betting ${bet} on {lines} lines. Total bet is ${total_bet}.")
+
+    slots =  get_slot_machine_spin(ROWS, COLS, symbol_count)
+    print_slot_machine(slots)
 
 main()
