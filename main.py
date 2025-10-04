56ea7717b34ab4fab001481330ebf2e0c7d03259
Georgi Komov <69540742+gkomov123@users.noreply.github.com>
2025-10-04 06:23:20 +0300

    added prints

--- main.py
+++ main.py
@@ -132,7 +132,8 @@ def main():
 
     slots =  get_slot_machine_spin(ROWS, COLS, symbol_count)
     print_slot_machine(slots)
-    winnigs = check_winnings(slots, lines, bet, symbol_values)
-    print(f"You won ${winnigs}.")
+    winnings, winning_lines = check_winnings(slots, lines, bet, symbol_values)
+    print(f"You won ${winnings}.")
+    print(f"You won on lines:", *winning_lines)
 
 main()
